1ff747002e1360efc0108c01977b3af2906a8c54
Paul Roberts <paul.roberts@britishgas.co.uk>
2023-08-10 15:35:10 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,45 +1,40 @@
 // Supabase Initialization
 const supabaseUrl = 'https://airfdancdzkaxebikanm.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpcmZkYW5jZHprYXhlYmlrYW5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE2NjQ0OTksImV4cCI6MjAwNzI0MDQ5OX0.GnIe2Zdg3d5Vv00a201j7GKilZ0pGicrcgDvRd5sPGM';
-const supabase = Supabase.createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 const questionContainer = document.getElementById('question-container');
 const nextButton = document.getElementById('next');
-let currentQuestionIndex, score = 0;
+let shuffledQuestions, currentQuestionIndex, score = 0;
 
 const questions = [
     {
-        question: 'Who developed the GPT-4 model?',
+        question: 'What does AI stand for?',
         answers: [
-            { text: 'Google', correct: false },
-            { text: 'Facebook', correct: false },
-            { text: 'OpenAI', correct: true },
-            { text: 'Microsoft', correct: false }
+            { text: 'Artificial Intelligence', correct: true },
+            { text: 'Automated Ignition', correct: false },
+            { text: 'Active Interface', correct: false },
+            { text: 'Agriculture Industry', correct: false }
         ]
     },
-    //... [the rest of the questions here]
+    // Additional questions here
 ];
 
 // Function to start the quiz
 function startQuiz() {
+    shuffledQuestions = questions.sort(() => Math.random() - 0.5);
     currentQuestionIndex = 0;
     nextButton.addEventListener('click', () => {
-        const selectedAnswer = document.querySelector('input[name="answer"]:checked');
-        if (selectedAnswer) {
-            if (questions[currentQuestionIndex].answers[selectedAnswer.value].correct) {
-                score++;
-            }
-            currentQuestionIndex++;
-            setNextQuestion();
-        }
+        currentQuestionIndex++;
+        setNextQuestion();
     });
     setNextQuestion();
 }
 
 // Function to set the next question
 function setNextQuestion() {
-    if (currentQuestionIndex < questions.length) {
-        showQuestion(questions[currentQuestionIndex]);
+    if (currentQuestionIndex < shuffledQuestions.length) {
+        showQuestion(shuffledQuestions[currentQuestionIndex]);
     } else {
         endQuiz();
     }
@@ -47,10 +42,8 @@ function setNextQuestion() {
 
 // Function to show the current question
 function showQuestion(question) {
-    questionContainer.innerHTML = `<div>${question.question}</div>`;
-    question.answers.forEach((answer, index) => {
-        questionContainer.innerHTML += `<label><input type="radio" name="answer" value="${index}">${answer.text}</label>`;
-    });
+    questionContainer.innerText = question.question;
+    // Add your logic to display the answer choices here
 }
 
 // Function to end the quiz
@@ -96,3 +89,4 @@ async function displayLeaderboard() {
 
 // Start the quiz when the page loads
 startQuiz();
+
