f92f3a416d5eed611a2c5727a37fb07cfb406791
Paul Roberts <paul.roberts@britishgas.co.uk>
2023-08-10 17:07:15 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,27 +1,43 @@
 // Supabase Initialization
 const supabaseUrl = 'https://airfdancdzkaxebikanm.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpcmZkYW5jZHprYXhlYmlrYW5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE2NjQ0OTksImV4cCI6MjAwNzI0MDQ5OX0.GnIe2Zdg3d5Vv00a201j7GKilZ0pGicrcgDvRd5sPGM';
+const supabaseAnonKey = '...';  // Make sure to have your actual Anon Key here
 const { createClient } = window.supabase;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
 const questionContainer = document.getElementById('question-container');
 const nextButton = document.getElementById('next');
 let shuffledQuestions, currentQuestionIndex, score = 0;
 
-    
 const questions = [
     {
         question: 'What does AI stand for?',
         answers: [
             { text: 'Artificial Intelligence', correct: true },
             { text: 'Amazing Interface', correct: false },
-            
+            // Add other choices if you want
         ]
     },
-    
+    {
+        question: 'Which of the following is a programming language?',
+        answers: [
+            { text: 'Python', correct: true },
+            { text: 'Snake', correct: false },
+            { text: 'Lizard', correct: false },
+            { text: 'Gecko', correct: false }
+        ]
+    },
+    {
+        question: 'Which company developed the Android operating system?',
+        answers: [
+            { text: 'Apple', correct: false },
+            { text: 'Microsoft', correct: false },
+            { text: 'Google', correct: true },
+            { text: 'Samsung', correct: false }
+        ]
+    }
+    // Add more questions here
 ];
-// Additional questions here
 
-// Function to start the quiz
 function startQuiz() {
     shuffledQuestions = questions.sort(() => Math.random() - 0.5);
     currentQuestionIndex = 0;
@@ -30,50 +46,8 @@ function startQuiz() {
         setNextQuestion();
     });
     setNextQuestion();
-
-    function selectAnswer(e) {
-    const selectedButton = e.target;
-    const correct = selectedButton.dataset.correct;
-    if (correct) {
-        score++;
-    }
-    // Optionally: remove all buttons or disable them to prevent multiple selections
-}
-function selectAnswer(e) {
-    const selectedButton = e.target;
-    const correct = selectedButton.dataset.correct;
-    if (correct) {
-        score++;
-    }
-    // Once an answer is selected, disable all answer buttons to prevent multiple selections
-    Array.from(questionContainer.children).forEach(button => {
-        if (button.tagName === 'BUTTON') {
-            button.disabled = true;
-        }
-    });
 }
 
-function showQuestion(question) {
-    questionContainer.innerHTML = ''; // clear previous content
-    
-    const questionElem = document.createElement('div');
-    questionElem.innerText = question.question;
-    questionContainer.appendChild(questionElem);
-    
-    question.answers.forEach(answer => {
-        const btn = document.createElement('button');
-        btn.innerText = answer.text;
-        btn.classList.add('btn');
-        if (answer.correct) {
-            btn.dataset.correct = answer.correct;
-        }
-        btn.addEventListener('click', selectAnswer);
-        questionContainer.appendChild(btn);
-    });
-}
-}
-
-// Function to set the next question
 function setNextQuestion() {
     if (currentQuestionIndex < shuffledQuestions.length) {
         showQuestion(shuffledQuestions[currentQuestionIndex]);
@@ -82,67 +56,37 @@ function setNextQuestion() {
     }
 }
 
-// Function to show the current question
 function showQuestion(question) {
-    questionContainer.innerHTML = ''; // clear previous content
-    
-    const questionElem = document.createElement('div');
-    questionElem.innerText = question.question;
-    questionContainer.appendChild(questionElem);
-    
+    questionContainer.innerText = question.question;
+    // Logic to display answer choices
     question.answers.forEach(answer => {
-        const btn = document.createElement('button');
-        btn.innerText = answer.text;
-        btn.classList.add('btn');
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
         if (answer.correct) {
-            btn.dataset.correct = answer.correct;
+            button.dataset.correct = answer.correct;
         }
-        btn.addEventListener('click', selectAnswer);
-        questionContainer.appendChild(btn);
+        button.addEventListener('click', selectAnswer);
+        questionContainer.appendChild(button);
     });
 }
 
-// Function to end the quiz
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    if (correct) {
+        score++;
+    }
+    Array.from(questionContainer.children).forEach(button => {
+        if (button.tagName === 'BUTTON') {
+            button.disabled = true;
+        }
+    });
+}
+
 async function endQuiz() {
-    // Display score to the user
+    // Display score
     document.getElementById("result").innerText = `Your score is: ${score}`;
 
     const username = prompt("Enter your name for the leaderboard:");
-    
-    let { data, error } = await supabase
-        .from('scores')
-        .insert([{ name: username, score: score }]);
-    
-    if (error) {
-        console.error("Error inserting score:", error);
-    }
-
-    // Display the leaderboard
-    displayLeaderboard();
-}
-
-// Function to display the leaderboard
-async function displayLeaderboard() {
-    let { data, error } = await supabase
-        .from('scores')
-        .select('*')
-        .order('score', { ascending: false })
-        .limit(50);
-
-    if (error) {
-        console.error("Error fetching leaderboard:", error);
-        return;
-    }
-
-    var leaderboardHtml = "<h2>Leaderboard</h2><ul>";
-    for (let i = 0; i < data.length; i++) {
-        leaderboardHtml += `<li>${data[i].name} - ${data[i].score}</li>`;
-    }
-    leaderboardHtml += "</ul>";
-
-    document.getElementById("leaderboard").innerHTML = leaderboardHtml;
-}
-
-// Start the quiz when the page loads
-startQuiz();
-
+    let { data, error } = await sup
